de28bda119d51734ef58e1e889947c65a4f8b63a
sammieliu <sammieliu@tencent.com>
2017-07-24 02:24:23 +0800

    aa

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Intel(R) Core(TM) i7-3840QM CPU @ 2.80GHz
 |getpid|2500w/s||
 |close|1800w/s||
 |socket|19w/s||
+|shmat|100w/s||
+|shmdt|100w/s||
--- src/perf_libc.cpp
+++ src/perf_libc.cpp
@@ -1,8 +1,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/shm.h>
 #include <unistd.h>
 
 #include "gtestx/gtestx.h"
+
+namespace {
+
+/**
+ * @brief 
+ * @class SysVShmTest
+ */
+class SysVShmTest: public ::testing::Test
+{
+protected:
+  virtual void SetUp() override {
+    shm_id_ = shmget(SHM_KEY_, 4096, IPC_CREAT | 0644);
+    ASSERT_NE(shm_id_, -1);
+  }
+
+  virtual void TearDown() override {
+    ASSERT_NE(shmctl(shm_id_, IPC_RMID, NULL), -1);
+  }
+
+  int shm_id_;
+private:
+  static constexpr key_t SHM_KEY_ = 0x10034;
+};
+
+}
 
 PERF_TEST(libc, getpid) {
   getpid();
@@ -20,4 +46,17 @@ PERF_TEST(libc, socket) {
 
 PERF_TEST(libc, close) {
   close(-1);
+}
+
+PERF_TEST_F(SysVShmTest, shmat_dt) {
+  void *addr = shmat(this->shm_id_, NULL, SHM_RDONLY);
+  ASSERT_TRUE(addr != reinterpret_cast<void *>(-1)) << PERF_ABORT;
+  ASSERT_EQ(shmdt(addr), 0) << PERF_ABORT;
+}
+
+PERF_TEST_F(SysVShmTest, shmat) {
+  shmat(this->shm_id_, NULL, SHM_RDONLY);
+}
+PERF_TEST_F(SysVShmTest, shmdt) {
+  shmdt(NULL);
 }
